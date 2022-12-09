38364232ad01ba81407956b139aecf182f6a0b1e
IsisG13 <isissouzaguimaraes@gmail.com>
2022-12-09 18:08:22 -0300

    arrumando detalhes footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer class="text-center lg:text-left">
-      <div class="bg-gray-200 w-full text-gray-700 text-center p-4 fixed bottom-0">
+    <footer className="text-center lg:text-left">
+      <div className="bg-gray-200 w-full text-gray-700 text-center p-4 fixed bottom-0">
         © 2022 Criado por:{' '}
-        <a class="text-gray-800" href="https://github.com/MarcellyGuimaraes">
+        <a
+          className="text-gray-800"
+          href="https://github.com/MarcellyGuimaraes"
+        >
           Marcelly Guimarães ^^
         </a>
       </div>
