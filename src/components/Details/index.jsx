980cf74ecba34d733c9adc41f3b47f48c7077948
IsisG13 <isissouzaguimaraes@gmail.com>
2022-12-09 16:40:57 -0300

    arrumando modal detalhes

--- src/components/Details/index.jsx
+++ src/components/Details/index.jsx
@@ -7,55 +7,44 @@ const Details = ({ show, item, onClose }) => {
 
   return (
     <>
-      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-        <div className="relative w-auto my-6 mx-auto max-w-3xl">
-          {/*content*/}
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-            {/*header*/}
-            <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-              <h3 className="text-3xl font-semibold">
-                {item.volumeInfo.title}
-              </h3>
-              <button
-                className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                onClick={onClose}
-              >
-                <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                  ×
-                </span>
-              </button>
-            </div>
-            {/*body*/}
-            <div className="relative p-6 flex-auto">
-              <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                {item.volumeInfo.description}
-              </p>
-              <p>{item.volumeInfo.authors}</p>
-              <p>
-                <span>Publicado em: {item.volumeInfo.publishedDate}</span>
-              </p>
-            </div>
-            {/*footer*/}
-            <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-              <button
-                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={onClose}
-              >
-                Fechar
-              </button>
-              <button
-                className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={onClose}
-              >
-                Favoritar
-              </button>
+      <div className="fixed inset-0 z-10 overflow-y-auto">
+        <div
+          className="fixed inset-0 w-full h-full bg-black opacity-40"
+          onClick={onClose}
+        ></div>
+        <div className="flex items-center min-h-screen px-4 py-8">
+          <div className="relative w-full max-w-lg p-4 mx-auto bg-white rounded-md shadow-lg">
+            <div className="mt-3 sm:flex">
+              <div className="mt-2 text-center sm:ml-4 sm:text-left">
+                <h4 className="text-lg font-medium text-gray-800">
+                  {item.volumeInfo.title}
+                </h4>
+                <p className="mt-2 text-[15px] leading-relaxed text-gray-500">
+                  {item.volumeInfo.description}
+                  <br />
+                  <span>{item.volumeInfo.authors}</span>
+                  <br />
+                  <span>Publicado em: {item.volumeInfo.publishedDate}</span>
+                </p>
+                <div className="items-center gap-2 mt-3 sm:flex">
+                  <button
+                    className="w-full mt-2 p-2.5 flex-1 text-red-500 background-transparent font-bold uppercase rounded-md outline-none ring-offset-2 ring-red-600 focus:ring-2"
+                    onClick={onClose}
+                  >
+                    Fechar
+                  </button>
+                  {/* <button
+                    className="w-full mt-2 p-2.5 flex-1 text-gray-800 rounded-md outline-none border ring-offset-2 ring-indigo-600 focus:ring-2"
+                    onClick={onClose}
+                  >
+                    Favoritar
+                  </button> */}
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="opacity-10 fixed inset-0 z-40 bg-black"></div>
     </>
   )
 }
